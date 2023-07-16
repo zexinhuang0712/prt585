48897315f10cf00d783e01bd0a41ae5572e926bc
zexinhuang0712 <122250729+zexinhuang0712@users.noreply.github.com>
2023-07-16 18:44:59 +0930

    Add files via upload

---
+++ todo/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using todo.Data;
+using Microsoft.AspNetCore.ResponseCompression;
+using BlazorServerSignalRApp.Server.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+          new[] { "application/octet-stream" });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.UseResponseCompression();
+
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();
+
